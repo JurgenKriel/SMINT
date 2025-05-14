57025fa22ec42f8e02927238d639e1f66346683b
Jurgen <krielj@gmail.com>
2025-05-14 13:36:42 +1000

    Update __init__.py

--- smint/alignment/__init__.py
+++ smint/alignment/__init__.py
@@ -7,8 +7,31 @@ and for registering images using ST Align.
 
 from .st_align_wrapper import align_spatial_transcriptomics, load_alignment, save_alignment
 
+# Import Xenium-Metabolomics alignment functionality
+try:
+    from .xenium_metabolomics import (
+        align_xenium_to_metabolomics,
+        read_xenium_data,
+        read_sm_matrix,
+        visualize_alignment
+    )
+    XENIUM_METABOLOMICS_AVAILABLE = True
+except ImportError:
+    # STalign or other dependencies might be missing
+    XENIUM_METABOLOMICS_AVAILABLE = False
+
+# Only include available functionalities in __all__
 __all__ = [
     'align_spatial_transcriptomics',
     'load_alignment',
     'save_alignment'
 ]
+
+# Add Xenium-Metabolomics alignment if available
+if XENIUM_METABOLOMICS_AVAILABLE:
+    __all__.extend([
+        'align_xenium_to_metabolomics',
+        'read_xenium_data',
+        'read_sm_matrix',
+        'visualize_alignment'
+    ])
